aa4446095c3f3beb833fbf6e16e9e6a08ba6201e
Yukichi Kawada <yukichikawada@gmail.com>
2018-09-05 23:09:23 -0700

    Add success message for successful creation of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # Rails interprets this as user_url(@user)
     else                # COOOOL!
       render 'new'
